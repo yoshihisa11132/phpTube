512219d3dec1d5ed6ef0207c6c7990f98d7cbbb9
JF6DEU <152010892+JF6DEU@users.noreply.github.com>
2025-01-23 15:46:22 +0900

    create core files

--- settings/main.php
+++ settings/main.php
@@ -1,5 +1,6 @@
 <?php
-    $AS = [
-        ["gizoucookie"] = "gz" //偽造用cookieの名前
-        ["gizouOKname"] = "ok" //偽造用cookieの値
-    ]
+    $AS = array(
+        "gizoucookie" => "gz", //偽造用cookieの名前
+        "gizouOKname" => "ok", //偽造用cookieの値
+        "invidious" => "https://invidious.schenkel.eti.br" //invidiousの値
+    );
